597fed6c03d78a4606e0eaad35b6c52a7ff41988
lkoczyba <lukasz.koczyba@gmail.com>
2023-11-25 19:01:43 +0100

    create user global state

--- src/features/user/userName.jsx
+++
@@ -1,5 +0,0 @@
-function userName() {
-  return <div className="hidden text-sm font-semibold md:block">Name</div>;
-}
-
-export default userName;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import UserName from "../features/user/userName";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
@@ -11,7 +11,7 @@ function Header() {
 
       <SearchOrder />
 
-      <UserName />
+      <Username />
     </header>
   );
 }
