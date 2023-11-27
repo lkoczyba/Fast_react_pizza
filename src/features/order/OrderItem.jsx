b7cc78368507dcfc96bf0270509f90c29c69a3dc
lkoczyba <lukasz.koczyba@gmail.com>
2023-11-27 18:37:54 +0100

    update order status

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,8 +1,7 @@
 import { formatCurrency } from "../../utils/helpers";
 
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
+function OrderItem({ item, isLoadingIngredients, ingredients, x }) {
   const { quantity, name, totalPrice } = item;
-
   return (
     <li className="py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
@@ -11,6 +10,9 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? "Loading..." : ingredients?.join(", ")}
+      </p>
     </li>
   );
 }
