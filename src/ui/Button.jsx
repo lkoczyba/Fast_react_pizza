8bbb36843b0b59920f5b99af51961a5fe75bed44
lkoczyba <lukasz.koczyba@gmail.com>
2023-11-27 14:54:20 +0100

    implement cart functionality

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     " text-sm inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
@@ -9,6 +9,7 @@ function Button({ children, disabled, to, type }) {
     secondary:
       " text:sm focus:text-stone-800 hover:text-stone-800  border-2 px-4 py-2.5 md:px-6 md:py-3.5 border-stone-300 inline-block rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
     small: base + " py-2 md:px-5 md:py-2.5 px-4 text-xs",
+    round: base + " py-1 md:px-3.5 md:py-2 px-2.5 text-sm",
   };
 
   if (to)
@@ -16,6 +17,13 @@ function Button({ children, disabled, to, type }) {
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
+    );
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
     );
 
   return (
